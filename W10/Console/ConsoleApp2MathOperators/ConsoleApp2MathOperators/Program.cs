3f999f332bad6036f8d127f2908cb80b41af8fd1
TheBeachMaster <otienoken@outlook.com>
2016-12-30 22:29:44 +0300

    Succesful operation

--- W10/Console/ConsoleApp2MathOperators/ConsoleApp2MathOperators/Program.cs
+++ W10/Console/ConsoleApp2MathOperators/ConsoleApp2MathOperators/Program.cs
@@ -10,6 +10,47 @@ namespace ConsoleApp2MathOperators
     {
         static void Main(string[] args)
         {
+            double firstNumber, secondNumber;
+            string userName;
+           
+
+
+            Console.WriteLine("Enter your Username followed by Enter Key");
+
+            userName = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {userName}");
+
+            Console.WriteLine("Enter the first number to manipulate");
+
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Good , enter a second digit");
+
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Well, {userName} ,...");
+
+            double sum = firstNumber + secondNumber;
+            double product = firstNumber * secondNumber;
+            double divide = firstNumber / secondNumber;
+            double subtract = firstNumber - secondNumber;
+            double modulo = firstNumber % secondNumber;
+
+            
+
+            Console.WriteLine($"the sum is : {sum} \n" +
+                              $"the product is : {product} \n" +
+                              $"the division is : {divide} \n" +
+                              $"the difference is : {subtract} \n" + 
+                              $"the remainder is : {modulo} \n");
+            Console.WriteLine("Please press any key to continue");
+
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
