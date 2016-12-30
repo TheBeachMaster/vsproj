6dd1c842263e2f8c70ab6f48a029f78f1aabfbc0
TheBeachMaster <otienoken@outlook.com>
2016-12-30 19:43:18 +0300

    Played around with Verabtims

--- W10/Console/ConsoleApp2Types/ConsoleApp2Types/Program.cs
+++ W10/Console/ConsoleApp2Types/ConsoleApp2Types/Program.cs
@@ -16,6 +16,12 @@ namespace ConsoleApp2Types
             myInteger =  17;
             myString = "\"myInteger\" is";
             Console.WriteLine($"{myString} {myInteger}"); //Outputs "myInteger is 17"
+
+            string myVerbatim = @"This is a verbatim string:
+                                   1.It can span several lines
+                                   2.This is anoter line spanned by
+                                     the verbatim string";
+            Console.WriteLine($"{myVerbatim}");
             Console.ReadKey();        
         }
     }
