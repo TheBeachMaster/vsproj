70bdfc69855b6706d0be33c8cc4ba50229a1ba21
TheBeachMaster <otienoken@outlook.com>
2016-12-31 21:08:36 +0300

    Added Database File

---
+++ Web/ASP/AwesomeWFCRUD/dbo.Contacts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY, 
+    [ContactPerson] VARCHAR(50) NOT NULL, 
+    [ContactNo] VARCHAR(50) NOT NULL, 
+    [CountryID] INT NOT NULL, 
+    [StateID] VARCHAR(50) NOT NULL
+)
