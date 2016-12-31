86a37b4a5c15795480d965610f18fd2fe431b045
TheBeachMaster <otienoken@outlook.com>
2016-12-31 23:37:16 +0300

    Shit

---
+++ Web/ASP/AwesomeWFCRUD/AwesomeWFCRUD/Contact.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AwesomeWFCRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Fetch Countries
+
+        private List<Country> PopulateCountry()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                return dc.Countries.OrderBy(a => a.CountryName).ToList();
+            }
+        }
+
+    }
+}
--- Web/ASP/AwesomeWFCRUD/AwesomeWFCRUD/ExtendedModel/Contact.cs
+++ Web/ASP/AwesomeWFCRUD/AwesomeWFCRUD/ExtendedModel/Contact.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AwesomeWFCRUD.ExtendedModel
+namespace AwesomeWFCRUD
 {
-    public class Contact
+    public partial class Contact
     {
+        public string CountryName { get; set; }
+        public string StateName { get; set; }
     }
 }
