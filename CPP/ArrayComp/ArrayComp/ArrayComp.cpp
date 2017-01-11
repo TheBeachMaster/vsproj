d6084c9f47115968282e518aeee87a8618991cde
TheBeachMaster <otienoken@outlook.com>
2017-01-11 23:42:18 +0300

    Cleanups

--- CPP/ArrayComp/ArrayComp/ArrayComp.cpp
+++ CPP/ArrayComp/ArrayComp/ArrayComp.cpp
@@ -17,30 +17,21 @@ bool checkSimilarity(int arraya[], int arrayb[], int a, int b)
 
 int main()
 {
-	int arrayA[5], arrayB[5] , i =0; //Declare our two Arrays A and B and another variable i to count our Arrays
 
-	cout << "Enter 5 numbers of the First Array" << endl;
-
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> arrayA[i];
-	}
-
-	cout << "Enter 5 numbers of the Second Array" << endl;
-
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> arrayB[i];
-	}
-
-
-	int a = (sizeof(arrayA) / sizeof(arrayA[0]));
-	int b = (sizeof(arrayB) / sizeof(arrayB[0]));
-	cout << checkSimilarity(arrayA, arrayB, a, b);
-	 
-
-
-
-    //return 0;
 }
 
+/*
+cout << "Enter 5 numbers of the First Array" << endl;
+
+for (int i = 0; i < 5; i++)
+{
+cin >> arrayA[i];
+}
+
+cout << "Enter 5 numbers of the Second Array" << endl;
+
+for (int i = 0; i < 5; i++)
+{
+cin >> arrayB[i];
+}
+*/
