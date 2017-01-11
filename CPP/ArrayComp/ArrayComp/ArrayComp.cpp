ecf4ecfbead001131e95118d3d5d9b8a9e4ff59c
TheBeachMaster <otienoken@outlook.com>
2017-01-11 23:49:58 +0300

    Reverted Changes

--- CPP/ArrayComp/ArrayComp/ArrayComp.cpp
+++ CPP/ArrayComp/ArrayComp/ArrayComp.cpp
@@ -17,25 +17,23 @@ bool checkSimilarity(int arraya[], int arrayb[], int a, int b)
 
 int main()
 {
-	int arr[] = { 1, 2, 2 };
-	int arrb[] = { 1, 2, 1 };
-	int a = (sizeof(arr) / sizeof(arr[0]));
-	int b = (sizeof(arrb) / sizeof(arrb[0]));
-	std::cout << checkSimilarity(arr, arrb, a, b);
+	int arrayA[5], arrayB[5], i = 0;
+
+	cout << "Enter 5 numbers of the First Array" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arrayA[i];
+	}
+
+	cout << "Enter 5 numbers of the Second Array" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arrayB[i];
+	}
+	int a = (sizeof(arrayA) / sizeof(arrayA[0]));
+	int b = (sizeof(arrayB) / sizeof(arrayB[0]));
+	std::cout << checkSimilarity(arrayA, arrayB, a, b);
 }
 
-/*
-cout << "Enter 5 numbers of the First Array" << endl;
-
-for (int i = 0; i < 5; i++)
-{
-cin >> arrayA[i];
-}
-
-cout << "Enter 5 numbers of the Second Array" << endl;
-
-for (int i = 0; i < 5; i++)
-{
-cin >> arrayB[i];
-}
-*/
