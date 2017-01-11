00880cd247ede7abd76c36f696e5f5d69ef90157
TheBeachMaster <otienoken@outlook.com>
2017-01-11 23:24:23 +0300

    For the First Array

--- CPP/ArrayComp/ArrayComp/ArrayComp.cpp
+++ CPP/ArrayComp/ArrayComp/ArrayComp.cpp
@@ -12,6 +12,17 @@ int main()
 
 	cout << "Enter 5 numbers of the First Array" << endl;
 
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arrayA[i];
+	}
+
+	cout << "Enter 5 numbers of the Second Array" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arrayB[i];
+	}
 	 
     return 0;
 }
