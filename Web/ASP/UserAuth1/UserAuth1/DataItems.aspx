11e3cd84151b6910b235c6f67d2fcf136cd1b380
TheBeachMaster <otienoken@outlook.com>
2017-04-14 14:47:41 +0300

    Edited Page Load Defaults

---
+++ Web/ASP/UserAuth1/UserAuth1/DataItems.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataItems.aspx.cs" Inherits="UserAuth1.DataItems" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Arthur's Super Awesome Caffe</title>
+    
+    <link href="Contents/bootstrap-theme.min.css" rel="stylesheet" />
+    <link href="Contents/bootstrap.min.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/jQuery.js"></script>
+    <script src="Scripts/npm.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Welcome to Arthur's Magical Caffe</h2>
+            <asp:CheckBoxList ID="FoodItemsList" runat="server">
+
+            </asp:CheckBoxList>
+
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="BtnSubmit" runat="server" Height="34px" Text="Submit Order" Width="152px" CssClass="btn btn-lg btn-primary" OnClick="AddItems" />
+    </form>
+</body>
+</html>
---
+++ Web/ASP/UserAuth1/UserAuth1/DataItems.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UserAuth1
+{
+    public partial class DataItems : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Ensure Items load when the page is launched
+                this.PopulateItems();
+            }
+        }
+
+        protected void AddItems(object sender, EventArgs e)
+        {
+            //You can have the User's name added here to DB  for submission
+
+            //This Data Goes to a Separate Table
+
+        }
+
+        private void PopulateItems()
+        {
+            using (SqlConnection sqlConn = new SqlConnection())
+            {
+                sqlConn.ConnectionString = ConfigurationManager.ConnectionStrings["foodConstr"].ConnectionString;
+
+                using (SqlCommand sqlCmd = new SqlCommand())
+                {
+                    sqlCmd.CommandText = "SELECT * FROM FoodItems";
+                    sqlCmd.Connection = sqlConn;
+
+                    //Open Connection to SQL Server Instance
+
+                    sqlConn.Open();
+
+                    //Read Data and Nuke 
+                    using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+                    {
+                        while (sqlReader.Read()) //Read All Items
+                        {
+                            ListItem dataItems = new ListItem();
+                            dataItems.Text = sqlReader["Food Name"].ToString();
+                            dataItems.Value = sqlReader["Food Price"].ToString();
+                            dataItems.Selected = Convert.ToBoolean(sqlReader["Selected"]);
+                            FoodItemsList.Items.Add(dataItems);
+                        }
+                    }
+
+                    sqlConn.Close();
+                    //Close all connections
+                }
+            }
+        }
+    }
+}
