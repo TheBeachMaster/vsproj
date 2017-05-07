a57439ba17da2b27fc67de0f384f91a872b293df
TheBeachmaster <otienoken@outlook.com>
2017-05-07 23:22:33 +0300

    Loaded all restarants

--- Web/ASP/UserAuth1/UserAuth1/Menus.aspx.cs
+++ Web/ASP/UserAuth1/UserAuth1/Menus.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,50 @@ namespace UserAuth1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                this.GetRestaurants();
+            }
+        }
 
+        /// <summary>
+        /// Gets a list of restarants from the DB and populates them
+        /// main page.
+        /// This function is loaded first when the page loads
+        /// </summary>
+        private void GetRestaurants()
+        {
+            using (SqlConnection sqlConn = new SqlConnection())
+            {
+                sqlConn.ConnectionString = ConfigurationManager.ConnectionStrings["transact"].ConnectionString;
+
+                using (SqlCommand sqlCmd = new SqlCommand())
+                {
+                    sqlCmd.CommandText = "SELECT * FROM Restaurants";
+                    sqlCmd.Connection = sqlConn;
+
+                    //Open Connection to SQL Server Instance
+
+                    sqlConn.Open();
+
+                    //Read Data and Nuke 
+                    using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+                    {
+                        while (sqlReader.Read()) //Read All Items
+                        {
+                            ListItem restarants = new ListItem();
+
+                            restarants.Text = sqlReader["Restaurant Name"].ToString();
+                            restarants.Value = sqlReader["Restaurant Name"].ToString();
+                            restarants.Selected = false;
+                            RestaurantList.Items.Add(restarants);
+                        }
+                    }
+
+                    sqlConn.Close();
+                    //Close all connections
+                }
+            }
         }
     }
 }
