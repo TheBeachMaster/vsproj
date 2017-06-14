2a601ba43f891b767b41768c3784f0243983adc7
TheBeachmaster <otienoken@outlook.com>
2017-06-15 00:07:36 +0300

    New AT Project

---
+++ F#/Crawler/Crawler/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open FSharp.Data
+
+[<Literal>]
+let url = @"https://www.jumia.co.ke/desktop-computers/hp/"
+type HPData = HtmlProvider<url>
+
+let func() = 
+      let data = HPData.GetSample()
+      data.Lists.List10.Values
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
---
+++ Xamarin/Demo/Demo/Demo.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Demo.App());
+        }
+    }
+}
