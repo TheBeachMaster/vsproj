2de6c6b1d56bc09057a5f74e657de38a985d6b45
TheBeachmaster <otienoken@outlook.com>
2017-12-31 23:50:04 +0300

    Scaffold new MQTT.NET solution

---
+++ W10/Console/MQTT_net/MQTT_net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTT_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
