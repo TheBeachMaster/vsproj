b7b8f1dbe50c19e239759109a822ad1a9134ab29
TheBeachmaster <otienoken@outlook.com>
2018-02-01 14:45:04 +0300

    Add test Regex application

--- W10/Console/RegexString/RegexString/Program.cs
+++ W10/Console/RegexString/RegexString/Program.cs
@@ -11,21 +11,25 @@ namespace RegexString
     {
         static void Main(string[] args)
         {
-            string phoneNumberList = "+25472487654";
-            //string phoneNumberList = "+25472487654,+254734587654,+25471587654";
+            bool status = false;
+            bool stat = true;
+            //string phoneNumberList = "+4654";
+            string phoneNumberList = "+25472487654,+25473487654,+25471587654,+4654";
             string[] nums = phoneNumberList.Split(',');
             foreach (string num in nums)
             {
                 Console.WriteLine(num);
-                Console.WriteLine(IsPhoneNumber(num));
+                status =  IsPhoneNumber(num);
+                Console.WriteLine(status);
+                stat = stat & status;
             }
-            
+            Console.WriteLine("Overall: " +stat);
             Console.ReadLine();
         }
 
         private static bool IsPhoneNumber(string number)
         {
-                      return Regex.Match(number, @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$").Success && number.Length > 5;
+                      return Regex.Match(number, @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d{5,}$").Success;
         }
     }
 }
